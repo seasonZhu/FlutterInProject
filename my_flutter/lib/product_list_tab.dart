93855d982bcfbffd8e1f3aa1f78ff1f9fb40005e
zhujilong <zhujilong2007@sina.com>
2020-01-13 16:34:25 +0800

    购物车的显示构建

--- my_flutter/lib/product_list_tab.dart
+++ my_flutter/lib/product_list_tab.dart
@@ -2,20 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import 'model/app_state_model.dart';
+import 'product_row_item.dart'; 
 
 class ProductListTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        return CustomScrollView(
-          slivers: const <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Cupertino Store'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return CupertinoPageScaffold(
+     child: Consumer<AppStateModel>(
+       builder: (context, model, child) {
+         final products = model.getProducts();
+         return CustomScrollView(
+           semanticChildCount: products.length,
+           slivers: <Widget>[
+             CupertinoSliverNavigationBar(
+               largeTitle: Text('Cupertino Store'),
+             ),
+           ],
+         );
+       },
+     ),
+   );
+ }
 }
