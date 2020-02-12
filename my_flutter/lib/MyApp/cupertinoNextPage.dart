313964da5533f693e6a8e7ed152a75f9fc8a7646
zhujilong <zhujilong2007@sina.com>
2020-02-12 21:02:48 +0800

    改写CupertinoNextPage

--- my_flutter/lib/MyApp/cupertinoNextPage.dart
+++ my_flutter/lib/MyApp/cupertinoNextPage.dart
@@ -3,24 +3,44 @@ import 'package:flutter/material.dart';
 
 import 'controlView.dart';
 
-class CupertinoNextPage extends StatelessWidget {
+class CupertinoNextPage extends StatefulWidget {
+  @override
+  _CupertinoNextPageState createState() => _CupertinoNextPageState();
+}
+
+class _CupertinoNextPageState extends State<CupertinoNextPage> {
+  var _switchSelected = false;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(middle: Text("Next Page")),
       child: Center(
         child: CupertinoButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              CupertinoPageRoute(
-                //fullscreenDialog: true, //加上这句是present, 不加这句是push
-                builder: (context) => ControlView(),
-              ),
-            );
-          },
-          child: Text("cupertino Next Page"),
-        ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  fullscreenDialog: !_switchSelected, //加上这句是present, 不加这句是push
+                  builder: (context) => ControlView(),
+                ),
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("cupertino Next Page"),
+                CupertinoSwitch(
+                  value: _switchSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      _switchSelected = value;
+                    });
+                  },
+                ),
+              ],
+            )),
       ),
     );
   }
