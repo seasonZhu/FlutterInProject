d436fbf2752eefccca98ff47ef7e9fe0b6c9cec7
zhujilong <zhujilong2007@sina.com>
2020-02-12 16:34:28 +0800

    添加代码,优化功能

---
+++ my_flutter/lib/MyApp/controlView.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class ControlView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ControlView();
+}
+
+class _ControlView extends State<ControlView> {
+  bool _switchSelected = true;
+  bool _checkboxSelected = true;
+  String _inputString = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("ControlView"),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CupertinoSwitch(
+            value: _switchSelected,
+            onChanged: (value) {
+              setState(() {
+                _switchSelected = value;
+              });
+            },
+          ),
+          // 下面的代码直接跑着直接就跪了
+          // Checkbox(value: _checkboxSelected, activeColor: Colors.red, onChanged: (value) {
+          //   setState(() {
+          //     _checkboxSelected = value;
+          //   });
+          // },),
+          RaisedButton(
+            onPressed: () {
+              print("点击了按钮");
+            },
+            child: Text("Material Button"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: CupertinoTextField(
+              placeholder: "请输入文字",
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                _inputString = value;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            width: 100,
+            child: CupertinoActivityIndicator(
+              animating: false,
+              radius: 10,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- my_flutter/lib/MyApp/cupertinoNextPage.dart
+++ my_flutter/lib/MyApp/cupertinoNextPage.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'controlView.dart';
 
 class CupertinoNextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text(
-          "Next Page"
-        )
-      ),
+      navigationBar: CupertinoNavigationBar(middle: Text("Next Page")),
       child: Center(
-        child: Text(
-          "cupertino Next Page"
+        child: CupertinoButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              CupertinoPageRoute(
+                //fullscreenDialog: true, //加上这句是present, 不加这句是push
+                builder: (context) => ControlView(),
+              ),
+            );
+          },
+          child: Text("cupertino Next Page"),
         ),
       ),
     );
