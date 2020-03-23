78674690e643ed47a0022b44085f57b7d36b0f8b
zhujilong <zhujilong2007@sina.com>
2020-03-23 17:32:43 +0800

    编写了一个基于网络请求的ListView

--- my_flutter/lib/MyApp/FutureBuilderView.dart
+++ my_flutter/lib/MyApp/FutureBuilderView.dart
@@ -1,6 +1,87 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class FutureBuilderView extends StatelessWidget {
+import 'package:dio/dio.dart';
+
+import 'package:my_flutter/models/chargeReport.dart';
+
+class FutureBuilderView extends StatefulWidget {
+  @override
+  _FutureBuilderViewState createState() => _FutureBuilderViewState();
+}
+
+class _FutureBuilderViewState extends State<FutureBuilderView> {
+  Dio _dio = Dio();
+
+  final String chargeReportApi =
+      "https://www.fastmock.site/mock/495b71fd9424c34326056713d9d3fb64/core/chargingReports/20355133";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("网络请求的list"),
+        ),
+        body: getContainer());
+  }
+
+  Container getContainer() {
+    return Container(
+      alignment: Alignment.center,
+      child: FutureBuilder(
+          future: getChargeReport(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            //请求完成
+            if (snapshot.connectionState == ConnectionState.done) {
+              ChargeReport model = snapshot.data;
+              //发生错误
+              if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              }
+              //请求成功，通过项目信息构建用于显示项目名称的ListView
+              return getSectionHeaderListView(model);
+              return getAnotherListView(model);
+              return ListView.builder(
+                itemCount: model.data.record.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text(model.data.record[index].startDay);
+                },
+              );
+            }
+            //请求未完成时弹出loading
+            return CircularProgressIndicator();
+          }),
+    );
+  }
+
+  ListView getAnotherListView(ChargeReport model) {
+    return ListView(
+      children: model.data.record
+          .map<Widget>((r) => ListTile(title: Text(r.startDay + r.hashCode.toString())))
+          .toList(),
+    );
+  }
+
+  Widget getSectionHeaderListView(ChargeReport model) {
+    return Column(children: 
+      <Widget>[
+        ListTile(title: Text("车辆保养记录")),
+        Expanded(
+          child: getAnotherListView(model),
+        ),
+      ]
+    );
+  }
+
+  Future<ChargeReport> getChargeReport() async {
+    Response response = await _dio.get(chargeReportApi);
+    Map<String, dynamic> data = response.data;
+    var model = ChargeReport.fromJson(data);
+    return ChargeReport.fromJson(response.data);
+  }
+}
+
+class FutureBuilderMockView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
